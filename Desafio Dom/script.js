004477e44276d5ae2464434444a4aa8fa36d044a
Marco <g33kz0rd@gmail.com>
2019-05-21 21:45:01 -0300

    Mejore el desafio DOM porque era horrible antes
    y andaba mal el remove :D

--- Desafio Dom/script.js
+++ Desafio Dom/script.js
@@ -2,11 +2,13 @@ const movieListDiv = document.getElementById("listaPeliculas");
 const movieNameInput = document.getElementById("nombrePelicula");
 const movieList = [];
 
+var idCount = 0;
+
 function createListItem(text) {
     const buttonItem = document.createElement("button");
     const buttonText = document.createTextNode("Borrar");
     buttonItem.appendChild(buttonText);
-    var index = movieList.length;
+    var index = idCount;
     buttonItem.onclick = function () { removeFromList(index) };
 
     const listItem = document.createElement("li");
@@ -14,17 +16,25 @@ function createListItem(text) {
     listItem.appendChild(itemText);
     listItem.appendChild(buttonItem);
     movieListDiv.append(listItem);
-    movieList.push(new MovieListItem(text, listItem));
+    movieList.push(new MovieListItem(idCount, text, listItem));
+    idCount++;
     clearInput();
 }
 
 function removeFromList(index) {
-    const element = movieList[index];
-    movieListDiv.removeChild(element.listItem);
-    movieList[index] = null;
+    for (let i = 0; i < movieList.length; i++) {
+        const element = movieList[i];
+
+        if (element.id != index)
+            continue;
+
+        movieListDiv.removeChild(element.listItem);
+        movieList.splice(i, 1);
+        return;
+    }
 }
 
-function clearInput(){
+function clearInput() {
     movieNameInput.value = "";
 }
 
@@ -36,7 +46,8 @@ function clearList() {
 }
 
 class MovieListItem {
-    constructor(nombre, listItem) {
+    constructor(id, nombre, listItem) {
+        this.id = id;
         this.nombre = nombre;
         this.listItem = listItem;
     }
